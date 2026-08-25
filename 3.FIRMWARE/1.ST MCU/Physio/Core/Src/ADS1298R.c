624ad2d256604574ba27f5e37b880a80250aa58a
moonylt <147577389+moonylt@users.noreply.github.com>
2026-08-25 12:30:06 +0800

    docs: translate ADS1298R.c comments to English

--- 3.FIRMWARE/1.ST MCU/Physio/Core/Src/ADS1298R.c
+++ 3.FIRMWARE/1.ST MCU/Physio/Core/Src/ADS1298R.c
@@ -5,9 +5,9 @@
 #define SPI_FLASH_CS_LOW() HAL_GPIO_WritePin(GPIOG, ECG_SSEL_Pin, GPIO_PIN_RESET)
 #define SPI_FLASH_CS_HIGH() HAL_GPIO_WritePin(GPIOG, ECG_SSEL_Pin, GPIO_PIN_SET)
 //Var
-unsigned char ADS1298R_REG[26];		//ADS1298R閿熶茎杈炬嫹閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷�
+unsigned char ADS1298R_REG[26];		//ADS1298R register array
 unsigned char buff[27]={};
-unsigned char ADS1298R_Cache[26];	//閿熸枻鎷烽敓鎹蜂紮鎷烽敓鏂ゆ嫹閿熸枻鎷�
+unsigned char ADS1298R_Cache[26];	//Register cache array
 
 
 extern unsigned char readdata[27];
@@ -29,7 +29,7 @@ ADS1298R_CHSETREG  ADS1298R_CH3CtlReg;
 //ADS1298R_RESP1			ADS1298R_Resp1			={RESP_DEMOD_EN1,RESP_MOD_EN,RESP_PH,RESP_CTRL};		//RSP1
 //ADS1298R_RESP2			ADS1298R_Resp2			={CALIB,FREQ,RLDREF_INT};														//RSP2
 
-#define CPU_FREQUENCY_MHZ 131 // STM32鏃堕挓涓婚
+#define CPU_FREQUENCY_MHZ 131 // STM32 main clock frequency
 void delay_us(__IO uint32_t delay)
 {
 
@@ -85,29 +85,29 @@ void ADS1298R_Init(void)
 //		SPI_FLASH_CS_LOW();
 }
 
-//volatile unsigned char ADS1298R_recive_flag=0;	//閿熸枻鎷烽敓鎹疯鎷峰彇閿熸枻鎷锋潃閿熻锟�
-//volatile unsigned char ADS1298R_Cache[26];	//閿熸枻鎷烽敓鎹蜂紮鎷烽敓鏂ゆ嫹閿熸枻鎷�
+//volatile unsigned char ADS1298R_recive_flag=0;	//receive-complete flag
+//volatile unsigned char ADS1298R_Cache[26];	//Register cache array
 
 
 //void EXTI9_5_IRQHandler(void)
 //{
 //	
-////		if(EXTI->IMR&EXTI_Line8 && ADS_DRDY==0)//閿熸枻鎷烽敓鎹锋枻鎷烽敓鏂ゆ嫹閿熷彨璁规嫹
+////		if(EXTI->IMR&EXTI_Line8 && ADS_DRDY==0)//Data ready
 ////		{		
 ////				EXTI_ClearITPendingBit(EXTI_Line8); 	
-////				ADS1298R_Read_Data((INT8U*)ADS1298R_Cache);//閿熸枻鎷烽敓鎹峰瓨鍒�9閿熻鑺備紮鎷烽敓鏂ゆ嫹閿熸枻鎷�
+////				ADS1298R_Read_Data((INT8U*)ADS1298R_Cache);//Read 9 bytes of data
 ////				ADS1298R_recive_flag=1;
 ////		}	
 //}
 
 
 
-//閿熸枻鎷峰彇72浣嶉敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹1100+LOFF_STAT[4:0]+GPIO[1:0]+13閿熸枻鎷�0+2CHx24浣嶉敓鏂ゆ嫹閿熸枻鎷�9閿熻鏂ゆ嫹
-//	1100	LOFF_STAT[4			3			2			1			0	]	//閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷锋皭閿熸枻鎷烽敓杈冿拷閿熸枻鎷稬OFF_STAT閿熶茎杈炬嫹閿熸枻鎷烽敓鏂ゆ嫹
+//Read 72 bits of data: 1100 + LOFF_STAT[4:0] + GPIO[1:0] + 13 zeros + 2CH x 24-bit data, 9 bytes in total
+//	1100	LOFF_STAT[4			3			2			1			0	]	//The bits that follow are the LOFF_STAT status bits
 //									RLD		1N2N	1N2P	1N1N	1N1P	
-//	閿熸枻鎷�	C0 00 00 FF E1 1A FF E1 52
+//Example: C0 00 00 FF E1 1A FF E1 52
 
-void ADS1298R_Read_Data(unsigned char *data)//72M鏃堕敓鏂ゆ嫹閿熼摪鐚存嫹閿熸枻鎷烽敓鏂ゆ嫹鏃堕敓鏂ゆ嫹绾�10us  8M鏃堕敓鏂ゆ嫹閿熸枻鎷� 閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷锋椂閿熸枻鎷风害 100us
+void ADS1298R_Read_Data(unsigned char *data)//At a 72 MHz clock the read takes about 10 us; at 8 MHz, about 100 us
 {		
 //		unsigned char i;
 //		uint8_t txdata=0x00;
@@ -131,7 +131,7 @@ void ADS1298R_Read_Data(unsigned char *data)//72M鏃堕敓鏂ゆ嫹閿熼摪鐚�
 }
 
 
-//閿熸枻鎷烽敓鐭瘎杈炬嫹閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷�
+//Set up the register buffer
 void ADS1298R_SET_REGBUFF(void)
 {   
 
@@ -211,36 +211,36 @@ void ADS1298R_WR_REGS(unsigned char reg,unsigned char len,unsigned char *data)
 }
 
 
-//閿熶茎杈炬嫹閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷峰啓閿熸枻鎷锋媷閿熸枻鎷烽敓锟�
+//Write the register buffer into the registers
 void ADS1298R_WRITE_REGBUFF(void)
 {
 //		unsigned char i,res=0;
-//		unsigned char REG_Cache[12];	//閿熻姤鍌ㄩ敓渚ヨ揪鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹
-//		ADS1298R_SET_REGBUFF();//閿熸枻鎷烽敓鐭瘎杈炬嫹閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷�
-//		ADS1298R_WR_REGS(WREG|CONFIG1,11,ADS1298R_REG+1);//閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鍙揪鎷烽敓渚ヨ揪鎷烽敓鏂ゆ嫹
+//		unsigned char REG_Cache[12];	//Register cache data
+//		ADS1298R_SET_REGBUFF();//Set up the register buffer
+//		ADS1298R_WR_REGS(WREG|CONFIG1,11,ADS1298R_REG+1);//Write the data into the registers
 //		DelayMs(10);		
-//		ADS1298R_WR_REGS(RREG|ID,12,REG_Cache);//閿熸枻鎷烽敓渚ヨ揪鎷烽敓鏂ゆ嫹
+//		ADS1298R_WR_REGS(RREG|ID,12,REG_Cache);//Read back the register data
 //		DelayMs(10);	
 //		
 //	#ifdef DEBUG_ADS1298R	
 //		printf("WRITE REG:\r\n");
-//		for(i=0;i<12;i++	)//瑕佸啓閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷�
+//		for(i=0;i<12;i++	)//Data to write
 //				printf("%d %x\r\n",i,ADS1298R_REG[i]);	
 //		printf("READ REG:\r\n");
 //	#endif	
 //	
 //	
-//		for(i=0;i<12;i++	)	//閿熸枻鎷烽敓渚ヨ揪鎷烽敓鏂ゆ嫹
+//		for(i=0;i<12;i++	)	//Read back the register data
 //		{						
 //				if(ADS1298R_REG[i] != REG_Cache[i])
 //				{
-//						if(i!= 0 && i!=8 && i != 11)	//0 8 閿熸枻鎷�11閿熸枻鎷稩D 閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹閿燂拷GPIO閿熸枻鎷烽敓锟�
+//						if(i!= 0 && i!=8 && i != 11)	//0, 8 and 11 are the ID and GPIO registers
 //								res=1;
 //						else
 //								continue;
 //				}					
 //			#ifdef DEBUG_ADS1298R
-//				printf("%d %x\r\n",i,REG_Cache[i]); //閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷�
+//				printf("%d %x\r\n",i,REG_Cache[i]); //Read back the register data
 //			#endif
 //		}	
 
@@ -274,54 +274,54 @@ void ADS1298R_PowerOnInit(void)
 
 
 
-//閿熸枻鎷烽敓鏂ゆ嫹閫氶敓鏂ゆ嫹1閿熻妭璇ф嫹1mV閿熸枻鎷烽敓鏂ゆ嫹閿熻剼鐚存嫹
-void ADS1298R_Single_Test(void) //娉ㄩ敓鏂ゆ嫹1292R閿熸枻鎷烽敓鍓跨尨鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹閫氶敓鏂ゆ嫹涓�閿熸枻鎷烽敓鑺傝鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鑴氬彿璇ф嫹閿熸枻鎷烽敓鏂ゆ嫹閿熸帴甯嫹娆わ拷閿熸枻鎷烽敓鏂ゆ嫹鍙敓杞垮尅鎷烽�氶敓鏂ゆ嫹2閿熸枻鎷烽敓缂达綇鎷�1292閿熸枻鎷烽敓鏂ゆ嫹褰遍敓鏂ゆ嫹
+//Set channel 1 to the internal 1 mV test signal
+void ADS1298R_Single_Test(void) //Note: the ADS1292R test signal is generated internally; only the latched mirror data of the ADS1292 can be read out
 {
 //		unsigned char res=0;
-//		ADS1298R_Config2.Int_Test = INT_TEST_ON;//閿熸枻鎷烽敓鑺傝鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鑴氱尨鎷�
-//		ADS1298R_Ch1set.MUX=MUX_Test_signal;//閿熸枻鎷烽敓鏂ゆ嫹閿熻剼鐚存嫹閿熸枻鎷烽敓鏂ゆ嫹
-//		ADS1298R_Ch2set.MUX=MUX_Test_signal;//閿熸枻鎷烽敓鏂ゆ嫹閿熻剼鐚存嫹閿熸枻鎷烽敓鏂ゆ嫹
+//		ADS1298R_Config2.Int_Test = INT_TEST_ON;//Enable the internal test signal
+//		ADS1298R_Ch1set.MUX=MUX_Test_signal;//Route the test signal to the channel input
+//		ADS1298R_Ch2set.MUX=MUX_Test_signal;//Route the test signal to the channel input
 //		
-//		if(ADS1298R_WRITE_REGBUFF())//鍐欓敓鏂ゆ嫹鎷囬敓鏂ゆ嫹閿燂拷
+//		if(ADS1298R_WRITE_REGBUFF())//Error writing data
 //				res=1;
 //		DelayMs(10);
 //		return res;		
 }
-//閿熸枻鎷烽敓鏂ゆ嫹閿熻妭璇ф嫹閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹
+//Noise test
 void ADS1298R_Noise_Test(void)
 {
 //		unsigned char res=0;
-//		ADS1298R_Config2.Int_Test = INT_TEST_OFF;//閿熸枻鎷烽敓鑺傝鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鑴氱尨鎷�
-//		ADS1298R_Ch1set.MUX = MUX_input_shorted;//閿熸枻鎷烽敓鏂ゆ嫹閿熼摪锟�
-//		ADS1298R_Ch2set.MUX = MUX_input_shorted;//閿熸枻鎷烽敓鏂ゆ嫹閿熼摪锟�
+//		ADS1298R_Config2.Int_Test = INT_TEST_OFF;//Disable the internal test signal
+//		ADS1298R_Ch1set.MUX = MUX_input_shorted;//Input shorted
+//		ADS1298R_Ch2set.MUX = MUX_input_shorted;//Input shorted
 
-//		if(ADS1298R_WRITE_REGBUFF())//鍐欓敓鏂ゆ嫹鎷囬敓鏂ゆ嫹閿燂拷
+//		if(ADS1298R_WRITE_REGBUFF())//Error writing data
 //				res=1;	
 //		DelayMs(10);			
 //		return res;			
 }
 
-//閿熸枻鎷烽敓鏂ゆ嫹閿熻剼鍙烽噰纭锋嫹妯″紡
+//Set normal signal acquisition mode
 void ADS1298R_Single_Read(void)
 {
 
 //		SPI_FLASH_CS_LOW();
 //		delay_us(1000);
 		HAL_Delay(20);
-		ADS1298R_Send_CMD(RDATAC); 	//閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹妯″紡
+		ADS1298R_Send_CMD(RDATAC); 	//Continuous data read mode
 		HAL_Delay(20);
-//		ADS1298R_Send_CMD(START);	//閿熸枻鎷烽敓閰靛尅鎷峰閿熸枻鎷烽敓鏂ゆ嫹杞敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹鏁堥敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹START閿熸枻鎷烽敓鑴氾綇鎷�
+//		ADS1298R_Send_CMD(START);	//Send START to begin conversions
 //		delay_us(10);
 //		SPI_FLASH_CS_HIGH();
 }
 
-//閿熸枻鎷烽敓鏂ゆ嫹ADS1298R閿熺即纭锋嫹閿熸枻鎷峰紡
+//Set the data acquisition mode
 void Set_ADS1298R_Collect(unsigned char mode)
 {
 //		unsigned char res;
 //
 //		delay_us(10);
-//		switch(mode)//閿熸枻鎷烽敓鐭噰纭锋嫹閿熸枻鎷峰紡
+//		switch(mode)//Select the acquisition mode
 //		{
 //				case 0:
 //					res =ADS1298R_Single_Read();
@@ -333,10 +333,10 @@ void Set_ADS1298R_Collect(unsigned char mode)
 //					res =ADS1298R_Noise_Test();
 //				break;
 //		}
-//		if(res)return 1;			//閿熶茎杈炬嫹閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷峰け閿熸枻鎷�
-//		ADS1298R_Send_CMD(RDATAC); 	//閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹妯″紡
+//		if(res)return 1;			//Acquisition mode configuration failed
+//		ADS1298R_Send_CMD(RDATAC); 	//Continuous data read mode
 //		delay_us(10);
-//		ADS1298R_Send_CMD(START);	//閿熸枻鎷烽敓閰靛尅鎷峰閿熸枻鎷烽敓鏂ゆ嫹杞敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹鏁堥敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹START閿熸枻鎷烽敓鑴氾綇鎷�
+//		ADS1298R_Send_CMD(START);	//Send START to begin conversions
 //		delay_us(10);
 //		return 0;
 }
